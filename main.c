4744f08af2adc773182597eccc9513071c8671dc
Sena Nur Bakır <150732354+ssenanb@users.noreply.github.com>
2025-05-21 01:55:20 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,71 +1,23 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
 #include "i2c_lcd.h"
-/* USER CODE END Includes */
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
 
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc;
-
 I2C_HandleTypeDef hi2c1;
-
 TIM_HandleTypeDef htim2;
 
-/* USER CODE BEGIN PV */
-I2C_LCD_HandleTypeDef hlcd;
-/* USER CODE END PV */
+I2C_LCD_HandleTypeDef hlcd; /* LCD defined */
 
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_ADC_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_TIM2_Init(void);
-/* USER CODE BEGIN PFP */
 
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
 uint32_t leftLDR;
 uint32_t rightLDR;
 
-uint32_t readLDR(uint32_t channel){
+uint32_t readLDR(uint32_t channel{  /* Get LDR Values */
 
 	HAL_ADC_Start(&hadc);
 	HAL_ADC_PollForConversion(&hadc, HAL_MAX_DELAY);
@@ -74,44 +26,20 @@ uint32_t readLDR(uint32_t channel){
 	return channel;
 }
 
-uint16_t angle_to_ccr(uint8_t angle) {
+uint16_t angle_to_ccr(uint8_t angle) {  /* Calculate CCR Value For Servo Motor */
 	return 50 + (angle * (200 / 180));
 }
-/* USER CODE END 0 */
 
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
 
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
+	
   MX_GPIO_Init();
   MX_ADC_Init();
   MX_I2C1_Init();
   MX_TIM2_Init();
-  /* USER CODE BEGIN 2 */
 
   hlcd.hi2c = &hi2c1;
   hlcd.address = 0x4E;
@@ -120,55 +48,39 @@ int main(void)
 
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
 
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
+  rightLDR = readLDR(ADC_CHANNEL_1);
+  leftLDR = readLDR(ADC_CHANNEL_0);
 
-    /* USER CODE BEGIN 3 */
-	  rightLDR = readLDR(ADC_CHANNEL_1);
-	  leftLDR = readLDR(ADC_CHANNEL_0);
-
-	  if(leftLDR < 1000 && rightLDR > 1000){
-		  lcd_clear(&hlcd);
-		  lcd_puts(&hlcd,"Servo turned");
-		  lcd_gotoxy(&hlcd, 0, 1);
-		  lcd_puts(&hlcd, "to right !");
-		  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, angle_to_ccr(0));
-	  }else if(leftLDR > 1000 && rightLDR < 1000){
-		  lcd_clear(&hlcd);
-		  lcd_puts(&hlcd,"Servo turned");
-		  lcd_gotoxy(&hlcd, 0, 1);
-		  lcd_puts(&hlcd, "to left !");
-		  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, angle_to_ccr(180));
-	  }else{
-		  lcd_clear(&hlcd);
-		  lcd_puts(&hlcd,"Both LDR");
-		  lcd_gotoxy(&hlcd, 0, 1);
-		  lcd_puts(&hlcd, "brightened,too !");
-		  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, angle_to_ccr(90));
-	  }
-
+  if(leftLDR < 1000 && rightLDR > 1000){
+	lcd_clear(&hlcd);
+	lcd_puts(&hlcd,"Servo turned");
+	lcd_gotoxy(&hlcd, 0, 1);
+	lcd_puts(&hlcd, "to right !");
+	__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, angle_to_ccr(0));
+  }else if(leftLDR > 1000 && rightLDR < 1000){
+	lcd_clear(&hlcd);
+	lcd_puts(&hlcd,"Servo turned");
+	lcd_gotoxy(&hlcd, 0, 1);
+	lcd_puts(&hlcd, "to left !");
+	__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, angle_to_ccr(180));
+  }else{
+        lcd_clear(&hlcd);
+        lcd_puts(&hlcd,"Both LDR");
+        lcd_gotoxy(&hlcd, 0, 1);
+	lcd_puts(&hlcd, "brightened,too !");
+         __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, angle_to_ccr(90));
   }
-  /* USER CODE END 3 */
+ }
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -181,8 +93,6 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -201,26 +111,8 @@ void SystemClock_Config(void)
   }
 }
 
-/**
-  * @brief ADC Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_ADC_Init(void)
 {
-
-  /* USER CODE BEGIN ADC_Init 0 */
-
-  /* USER CODE END ADC_Init 0 */
-
-  ADC_ChannelConfTypeDef sConfig = {0};
-
-  /* USER CODE BEGIN ADC_Init 1 */
-
-  /* USER CODE END ADC_Init 1 */
-
-  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
-  */
   hadc.Instance = ADC1;
   hadc.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
   hadc.Init.Resolution = ADC_RESOLUTION_12B;
@@ -240,8 +132,6 @@ static void MX_ADC_Init(void)
     Error_Handler();
   }
 
-  /** Configure for the selected ADC regular channel to be converted.
-  */
   sConfig.Channel = ADC_CHANNEL_0;
   sConfig.Rank = ADC_RANK_CHANNEL_NUMBER;
   sConfig.SamplingTime = ADC_SAMPLETIME_239CYCLES_5;
@@ -250,34 +140,15 @@ static void MX_ADC_Init(void)
     Error_Handler();
   }
 
-  /** Configure for the selected ADC regular channel to be converted.
-  */
   sConfig.Channel = ADC_CHANNEL_1;
   if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN ADC_Init 2 */
-
-  /* USER CODE END ADC_Init 2 */
-
 }
 
-/**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_I2C1_Init(void)
 {
-
-  /* USER CODE BEGIN I2C1_Init 0 */
-
-  /* USER CODE END I2C1_Init 0 */
-
-  /* USER CODE BEGIN I2C1_Init 1 */
-
-  /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
   hi2c1.Init.Timing = 0x00201D2B;
   hi2c1.Init.OwnAddress1 = 0;
@@ -292,44 +163,23 @@ static void MX_I2C1_Init(void)
     Error_Handler();
   }
 
-  /** Configure Analogue filter
-  */
   if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
   {
     Error_Handler();
   }
 
-  /** Configure Digital filter
-  */
   if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN I2C1_Init 2 */
-
-  /* USER CODE END I2C1_Init 2 */
-
 }
 
-/**
-  * @brief TIM2 Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_TIM2_Init(void)
 {
-
-  /* USER CODE BEGIN TIM2_Init 0 */
-
-  /* USER CODE END TIM2_Init 0 */
-
   TIM_ClockConfigTypeDef sClockSourceConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
   TIM_OC_InitTypeDef sConfigOC = {0};
 
-  /* USER CODE BEGIN TIM2_Init 1 */
-
-  /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 480-1;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
@@ -363,66 +213,24 @@ static void MX_TIM2_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN TIM2_Init 2 */
-
-  /* USER CODE END TIM2_Init 2 */
   HAL_TIM_MspPostInit(&htim2);
-
 }
 
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_GPIO_Init(void)
 {
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
-
-  /* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
-
-  /* USER CODE BEGIN MX_GPIO_Init_2 */
-
-  /* USER CODE END MX_GPIO_Init_2 */
 }
 
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   while (1)
   {
   }
-  /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
 }
-#endif /* USE_FULL_ASSERT */
